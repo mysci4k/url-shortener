0a0b54e55ac9410d5895114733aa2fdbd9836f47
Adam Szamalek <szamalekadam@gmail.com>
2026-09-24 19:23:40 +0200

    feat: add findByShortCode method to repository

--- src/main/java/com/mysci4k/shortener/repository/ShortUrlRepository.java
+++ src/main/java/com/mysci4k/shortener/repository/ShortUrlRepository.java
@@ -3,5 +3,8 @@ package com.mysci4k.shortener.repository;
 import com.mysci4k.shortener.entity.ShortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
+    Optional<ShortUrl> findByShortCode(String shortCode);
 }
