6db9ea389c5c921802dda35ceaea402e2eb458cf
Adam Szamalek <szamalekadam@gmail.com>
2026-09-24 20:03:31 +0200

    feat: add expiredInDays field to ShortenRequest dto

--- src/main/java/com/mysci4k/shortener/dto/ShortenRequest.java
+++ src/main/java/com/mysci4k/shortener/dto/ShortenRequest.java
@@ -1,7 +1,9 @@
 package com.mysci4k.shortener.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 
 public record ShortenRequest(
         @NotBlank(message = "URL cannot be blank")
@@ -9,5 +11,9 @@ public record ShortenRequest(
                 regexp = "^https?://.+",
                 message = "URL must be a valid HTTP or HTTPS URL"
         )
-        String url
+        String url,
+
+        @Positive(message = "Expired in days must be a positive integer")
+        @Max(value = 365, message = "Expired in days must be less than or equal to 365 days")
+        Integer expiredInDays
 ) {}
