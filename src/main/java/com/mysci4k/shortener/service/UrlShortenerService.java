f5897f97deee49cbe1c0e33c0a8e9bcd3ddb3f1e
Adam Szamalek <szamalekadam@gmail.com>
2026-09-24 19:25:02 +0200

    feat: add createShortenedUrl method to service

--- src/main/java/com/mysci4k/shortener/service/UrlShortenerService.java
+++ src/main/java/com/mysci4k/shortener/service/UrlShortenerService.java
@@ -1,9 +1,13 @@
 package com.mysci4k.shortener.service;
 
+import com.mysci4k.shortener.dto.ShortenRequest;
+import com.mysci4k.shortener.dto.ShortenResponse;
+import com.mysci4k.shortener.entity.ShortUrl;
 import com.mysci4k.shortener.repository.ShortUrlRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +17,19 @@ public class UrlShortenerService {
 
     @Value("${app.base-url}")
     private String baseUrl;
+
+    @Transactional
+    public ShortenResponse createShortenedUrl(ShortenRequest request) {
+        Long id = repository.fetchNextId();
+        String shortCode = base62Encoder.encode(id);
+
+        ShortUrl entity = new ShortUrl(id, request.url(), shortCode);
+        repository.save(entity);
+
+        return new ShortenResponse(
+                baseUrl + "/" + shortCode,
+                entity.getOriginalUrl(),
+                shortCode
+        );
+    }
 }
