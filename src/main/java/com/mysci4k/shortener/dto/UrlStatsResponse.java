626bedee2d50ca7eade9fd45275d425587218b0f
Adam Szamalek <szamalekadam@gmail.com>
2026-09-24 15:09:02 +0200

    feat: add UrlStatsResponse DTO definition

---
+++ src/main/java/com/mysci4k/shortener/dto/UrlStatsResponse.java
@@ -0,0 +1,10 @@
+package com.mysci4k.shortener.dto;
+
+import java.time.LocalDateTime;
+
+public record UrlStatsResponse(
+        String shortCode,
+        String originalUrl,
+        long clickCount,
+        LocalDateTime createdAt
+) {}
