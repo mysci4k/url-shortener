f96a2a20c3e2ac31e5db17ede2e74b29862767b9
Adam Szamalek <szamalekadam@gmail.com>
2026-09-24 19:23:54 +0200

    feat: add fetchNextId query to repository

--- src/main/java/com/mysci4k/shortener/repository/ShortUrlRepository.java
+++ src/main/java/com/mysci4k/shortener/repository/ShortUrlRepository.java
@@ -2,9 +2,13 @@ package com.mysci4k.shortener.repository;
 
 import com.mysci4k.shortener.entity.ShortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
     Optional<ShortUrl> findByShortCode(String shortCode);
+
+    @Query(value = "SELECT nextval('short_url_id_seq')", nativeQuery = true)
+    Long fetchNextId();
 }
