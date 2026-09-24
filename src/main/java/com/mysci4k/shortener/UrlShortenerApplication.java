a6e0f07b58ff67d99cb64b867d2f0bf2688200e0
Adam Szamalek <szamalekadam@gmail.com>
2026-09-24 20:17:41 +0200

    feat: enable scheduling in application

--- src/main/java/com/mysci4k/shortener/UrlShortenerApplication.java
+++ src/main/java/com/mysci4k/shortener/UrlShortenerApplication.java
@@ -2,12 +2,12 @@ package com.mysci4k.shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class UrlShortenerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerApplication.class, args);
     }
-
 }
