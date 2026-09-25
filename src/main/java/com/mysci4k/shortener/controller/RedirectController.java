cd0107ec0ba398063e3243863157837c7c29a5a6
Adam Szamalek <szamalekadam@gmail.com>
2026-09-25 17:58:24 +0200

    feat: add rate limiting to URL endpoints

--- src/main/java/com/mysci4k/shortener/controller/RedirectController.java
+++ src/main/java/com/mysci4k/shortener/controller/RedirectController.java
@@ -1,5 +1,6 @@
 package com.mysci4k.shortener.controller;
 
+import com.mysci4k.shortener.ratelimit.RateLimited;
 import com.mysci4k.shortener.service.UrlShortenerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @RequiredArgsConstructor
 public class RedirectController {
     private final UrlShortenerService service;
 
     @GetMapping("/{shortCode}")
+    @RateLimited(capacity = 50, duration = 1, timeUnit = TimeUnit.MINUTES)
     public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String shortCode) {
         String originalUrl = service.getOriginalUrlAndTrack(shortCode);
 
--- src/main/java/com/mysci4k/shortener/controller/UrlController.java
+++ src/main/java/com/mysci4k/shortener/controller/UrlController.java
@@ -3,12 +3,15 @@ package com.mysci4k.shortener.controller;
 import com.mysci4k.shortener.dto.ShortenRequest;
 import com.mysci4k.shortener.dto.ShortenResponse;
 import com.mysci4k.shortener.dto.UrlStatsResponse;
+import com.mysci4k.shortener.ratelimit.RateLimited;
 import com.mysci4k.shortener.service.UrlShortenerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/v1/urls")
@@ -17,6 +20,7 @@ public class UrlController {
     private final UrlShortenerService service;
 
     @PostMapping
+    @RateLimited(capacity = 10, duration = 1, timeUnit = TimeUnit.MINUTES)
     public ResponseEntity<ShortenResponse> generateShortenedLink(@Valid @RequestBody ShortenRequest request) {
         ShortenResponse response = service.createShortenedUrl(request);
 
@@ -24,6 +28,7 @@ public class UrlController {
     }
 
     @GetMapping("/{shortCode}/stats")
+    @RateLimited(capacity = 30, duration = 1, timeUnit = TimeUnit.MINUTES)
     public ResponseEntity<UrlStatsResponse> retrieveUrlStats(@PathVariable String shortCode) {
         UrlStatsResponse response = service.getUrlStats(shortCode);
 
@@ -31,6 +36,7 @@ public class UrlController {
     }
 
     @DeleteMapping("/{shortCode}")
+    @RateLimited(capacity = 10, duration = 1, timeUnit = TimeUnit.MINUTES)
     public ResponseEntity<Void> deleteUrl(@PathVariable String shortCode) {
         service.deleteUrl(shortCode);
 
