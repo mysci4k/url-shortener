07d5be8102ca0dc3eb0238bacb3a6f13ee858b1c
Adam Szamalek <szamalekadam@gmail.com>
2026-09-25 18:30:36 +0200

    feat: enhance RateLimitExceededException with retryAfterSeconds

--- src/main/java/com/mysci4k/shortener/exception/RateLimitExceededException.java
+++ src/main/java/com/mysci4k/shortener/exception/RateLimitExceededException.java
@@ -1,7 +1,14 @@
 package com.mysci4k.shortener.exception;
 
+import lombok.Getter;
+
+@Getter
 public class RateLimitExceededException extends RuntimeException {
-    public RateLimitExceededException() {
+    private final long retryAfterSeconds;
+
+    public RateLimitExceededException(long retryAfterSeconds) {
         super("Rate limit exceeded");
+
+        this.retryAfterSeconds = retryAfterSeconds;
     }
 }
